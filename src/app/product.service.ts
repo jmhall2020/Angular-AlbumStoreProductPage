05ef9a83de20559c91e2562d3a862a85bd0c8c65
jmhall2020 <60669457+jmhall2020@users.noreply.github.com>
2020-09-01 14:12:21 -0500

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,7 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get(albumUrl)
+    return this._http.get(this._albumUrl)
   }
 
 }
